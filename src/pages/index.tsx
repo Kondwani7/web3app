9aa0cda1781071d69aebac1b43267da889000908
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-27 16:54:09 +0200

    minting on frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import mintABI from '../abi/mintABI.json'
-import { ethers, BrowserProvider, parseUnits } from 'ethers'
+import { ethers, BigNumberish, BrowserProvider, parseUnits } from 'ethers'
 import { useEffect, useState } from 'react'
 import { BigNumber } from '@ethersproject/bignumber'
 import { MetaMaskInpageProvider } from '@metamask/providers'
 //enable the window to read the metamask provider
 declare global {
-  interface Window{
-    ethereum?:MetaMaskInpageProvider
+  interface Window {
+    ethereum?: MetaMaskInpageProvider
   }
 }
 
@@ -20,7 +20,7 @@ const minAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 export default function Home() {
   const [accounts, setAccounts]: Array<any> = useState([]);
   //access metamask on a browser
-  async function connectAccounts(){
+  async function connectAccounts() {
     if (window.ethereum) {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts"
@@ -34,14 +34,14 @@ export default function Home() {
   })
   //minting function
   const [mintAmount, setMintAmount] = useState(1);
-  
+
 
   async function handleMint() {
     //follwed the new ethers v6 documentation: https://docs.ethers.org/v6-beta/getting-started/
     if (window.ethereum) {
       const provider = new ethers.BrowserProvider(window.ethereum);
       const signer = await provider.getSigner();
-      const contract= new ethers.Contract(
+      const contract = new ethers.Contract(
         minAddress,
         mintABI.abi,
         signer
@@ -49,7 +49,7 @@ export default function Home() {
       try {
         const response = await contract.mint(BigNumber.from(mintAmount))
         console.log(`response: ${response}`);
-      } catch(err){
+      } catch (err) {
         console.log(`error:${err}}`);
       }
     }
@@ -62,8 +62,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <div className='text-3xl text-blue-500'>
-          Web 3 app for minting
+        <div className=''>
+          The mint button
+          {accounts.length && (
+            <div className='space-x-2 flex items-center'>
+              <button className='bg-gray-200 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded' onClick={() => setMintAmount(mintAmount - 1)}>-</button>
+              {mintAmount}
+              <button className='bg-gray-200 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded' onClick={() => setMintAmount(mintAmount + 1)}>+</button>
+              <button className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded' onClick={handleMint}>Mint</button>
+            </div>
+          )}
         </div>
       </main>
     </>
